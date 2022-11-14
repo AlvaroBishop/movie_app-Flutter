67befec3d5b2955d588288cb8426494219b2a07b
AlvaroBishop <bishopgamma@gmail.com>
2022-11-13 23:01:27 -0600

    add SilverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,9 +6,42 @@ class DetailsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('DetailsScreen'),
+    return  const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.green,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: EdgeInsets.all(0),
+        centerTitle: true,
+        title: Container(
+          color: Colors.black12,
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          child: Text('movie.title', style: TextStyle( fontSize: 16 ),),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: AssetImage('assets/no-image.jpg'),
+          fit: BoxFit.cover,
+          ),
       ),
     );
   }
