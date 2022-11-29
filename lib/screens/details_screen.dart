b4b90ff9dde5f8ee3120929d6de039c08042d008
AlvaroBishop <bishopgamma@gmail.com>
2022-11-29 09:01:27 -0600

    add details screen with movie image and title

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-   
   const DetailsScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([_PosterAndTitle()]),
+          )
         ],
       ),
     );
@@ -35,14 +37,77 @@ class _CustomAppBar extends StatelessWidget {
           color: Colors.black12,
           width: double.infinity,
           alignment: Alignment.bottomCenter,
-          child: Text('movie.title', style: TextStyle( fontSize: 16 ),),
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
         ),
         background: FadeInImage(
-          placeholder: AssetImage('assets/loading.gif'), 
+          placeholder: AssetImage('assets/loading.gif'),
           image: AssetImage('assets/no-image.jpg'),
           fit: BoxFit.cover,
-          ),
+        ),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage(
+              'https://via.placeholder.com/200x300',
+            ),
+            height: 150,
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'movie.title',
+              style: textTheme.headline5,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            Text(
+              'movie.originalTitle',
+              style: textTheme.subtitle1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.star_outline,
+                  size: 15,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'movie.voteAverage',
+                  style: textTheme.caption,
+                )
+              ],
+            )
+          ],
+        )
+      ]),
     );
   }
 }
