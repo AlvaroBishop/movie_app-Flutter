bc4675a54ef2019a577207a637383ef3431f137e
AlvaroBishop <bishopgamma@gmail.com>
2022-12-01 09:00:49 -0600

    add movie model

--- lib/provider/movies_provider.dart
+++ lib/provider/movies_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:movie_app/models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '098b976e614662f5a3dde592a987dcc9';
@@ -20,9 +21,8 @@ class MoviesProvider extends ChangeNotifier {
         {'api_key': _apiKey, 'language': _language, 'page': '1'});
 
     final response = await http.get(url);
-    final Map<String, dynamic> decodedData = json.decode(response.body);
-    if (response.statusCode != 200) return print('error');
+    final nowPlayingResponse = ReqRespListado.fromJson(response.body);
 
-    print(decodedData['results']);
+    print(nowPlayingResponse.results[0].title);
   }
 }
